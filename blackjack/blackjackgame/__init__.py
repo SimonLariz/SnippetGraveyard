371e68e34b1aea50b8f4efa6554edfaf2dd13a9c
SimonLariz <simonlariz@csu.fullerton.edu>
2023-02-26 18:51:28 -0800

    added blackjack

---
+++ blackjack/blackjackgame/__init__.py
@@ -0,0 +1,14 @@
+# Simon Lariz
+# CPSC 386-02
+# 2023-02-26
+# simonlariz@csu.fullerton.edu
+# @SimonLariz
+#
+# Lab 00-02
+#
+# This is the init for the blackjack game package.
+#
+
+"""This is the init for the blackjack game package."""
+
+__all__ = ["cards", "player", "game"]
